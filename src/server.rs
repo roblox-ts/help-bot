03ca602212249fe0d9d1d7fcb04f80a38fa80034
OverHash <46231745+OverHash@users.noreply.github.com>
2022-11-19 16:51:28 +1300

    Switch to hyper over rouille (#3)

--- src/server.rs
+++ src/server.rs
@@ -1,13 +1,23 @@
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Request, Response, Server};
+use std::convert::Infallible;
+use std::net::SocketAddr;
+
+async fn ping_health(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    Ok(Response::new("Hello world!".into()))
+}
+
 pub async fn start_server() {
     println!("Starting server..");
 
-    rouille::start_server("0.0.0.0:8080", move |request| {
-        rouille::router!(request,
-            (GET) (/) => {
-                rouille::Response::text("Hello world!")
-            },
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
 
-            _ => rouille::Response::empty_404()
-        )
-    });
+    let make_svc = make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(ping_health)) });
+
+    let server = Server::bind(&addr).serve(make_svc);
+
+    // Run this server for... forever!
+    if let Err(e) = server.await {
+        eprintln!("Server error: {}", e);
+    }
 }
