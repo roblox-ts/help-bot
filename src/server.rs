066250bd5dbde0ceecf40760dd821321c336ba64
osyrisrblx <osyrisrblx@gmail.com>
2022-11-18 23:21:11 -0500

    Fix health check

--- src/server.rs
+++ src/server.rs
@@ -10,7 +10,7 @@ async fn ping_health(_req: Request<Body>) -> Result<Response<Body>, Infallible> 
 pub async fn start_server() {
     println!("Starting server..");
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
 
     let make_svc = make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(ping_health)) });
 
